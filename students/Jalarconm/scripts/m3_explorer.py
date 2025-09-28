b1b4d9c8ab90338ffe97d72b510d2a1c19b19fab
Juan Sebastian Alarcon Ramirez <sebastian.alarconrmz@gmail.com>
2025-09-27 20:06:06 -0500

    >=(

--- students/Jalarconm/scripts/m3_explorer.py
+++ students/Jalarconm/scripts/m3_explorer.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv(r'C:\Users\sarac\Portalkids\PortalKidsOperaciones\students\Jalarconm\data\bronze\products.csv', sep = ',')
+df = pd.read_csv(r'C:\Users\sarac\Portalkids\PortalKidsOperaciones\students\Jalarconm\data\bronze\orders.csv', sep = ',')
 
 print(df)
-df.shape
 print("shape",df.shape)
+print(df.head())
+print(df.dtypes)
+print(df.columns.tolist())
+print(len(df))
