a9b542fa46b1dbbb2a226c6003ec2e12d98b3b3a
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2025-09-28 10:54:47 -0500

    Update m3_explorer.py

--- students/Jalarconm/scripts/m3_explorer.py
+++ students/Jalarconm/scripts/m3_explorer.py
@@ -2,8 +2,8 @@ from pathlib import Path
 import pandas as pd
 
 df = pd.read_csv(Path("sources/orders_seed.csv"))
-print("shape =", df.shape) 
-print("columns =", df.columns.tolist())
+print("df.shape =", df.shape) 
+print("df.columns =", df.columns.tolist())
 print("Head:\n", df.head().to_string())
 print("Dtypes:\n", df.dtypes)
 
