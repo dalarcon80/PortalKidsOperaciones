06e9d4b2bb04b3a2b2cbd3b37f318bc798b9f1db
dalarcon80 <63214788+dalarcon80@users.noreply.github.com>
2025-09-27 20:33:58 -0500

    Update m3_explorer.py

--- students/Jalarconm/scripts/m3_explorer.py
+++ students/Jalarconm/scripts/m3_explorer.py
@@ -1,10 +1,2 @@
-import pandas as pd
-
-df = pd.read_csv(r'\PortalKidsOperaciones\students\Jalarconm\data\bronze\orders.csv', sep = ',')
-
-print(df)
-print("shape",df.shape)
-print(df.head())
-print(df.dtypes)
-print(df.columns.tolist())
-print(len(df))
+print("Shape:", df.shape)
+print("Columns:", df.columns.tolist())
